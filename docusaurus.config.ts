d1023400b52700ea931779a59614b6f60fa3b84c
Jack Walker <jack.walker@theadaptivenetwork.org>
2023-12-03 12:05:36 +1300

    updated docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,9 +15,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "Adaptive Network", // Usually your GitHub org/user name.
+  organizationName: "adaptive-network", // Usually your GitHub org/user name.
   projectName: "process", // Usually your repo name.
-  deploymentBranch: "gh-pages",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
