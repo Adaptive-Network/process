955a02aafa5f20db630439c50b7afac5bf3f06b6
Jack Walker <jack.walker@theadaptivenetwork.org>
2023-12-03 11:17:43 +1300

    added http to deployment URL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: "img/logo_v2_icon_green_small.ico",
 
   // Set the production url of your site here
-  url: "process.theadaptivenetwork.org",
+  url: "https://process.theadaptivenetwork.org",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
